af00decf0781d9cc50110bc10741a376352881d7
Utsav790 <138883441+Utsav790@users.noreply.github.com>
2024-05-16 20:16:36 +0530

    Add files via upload

---
+++ question-7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int a=3;
+    char b='A';
+   float c=6.7;
+    printf("values of three variables=%d %c %f",a,b,c);
+    getch();
+    return 0;
+}
